02a73a29c7ff37012bc9bb0858a53fec599e2683
Jarek <jarekgab1@gmail.com>
2018-11-14 20:19:38 +0100

    Fixed ShoppingListActivity. V3

--- app/src/main/java/pl/nauka/jarek/udemy_mg/ShoppingListActivity.java
+++ app/src/main/java/pl/nauka/jarek/udemy_mg/ShoppingListActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
@@ -152,6 +154,28 @@ public class ShoppingListActivity extends AppCompatActivity {
     public void onClickItemDelete() {
         if (!itemName.getText().toString().equals("Podaj nazwę produktu"))
         itemName.setText("");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_shopping_list_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
 
